9ec062237d33854bf7aedf9ca6f9110ae1a68f44
Nerea Riveiro <nereaironhack@gmail.com>
2023-06-03 19:46:12 +0200

    más pruebas st mejores jeje

--- pruebasmain.py
+++ pruebasmain.py
@@ -1,7 +1,33 @@
 import streamlit as st
 
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+
 def main():
     st.title("Pruebas")
 
 if __name__ == '__main__':
     main()
+
+
+st.header("Go vegan?")
+st.subheader("Discover your food choices ecological impact")
+st.text("Are you ready for some fun?")
+
+
+nombre = st.text_input("What's your name?")
+st.write("¡Hi,", nombre, "!")
+
+
+
+opcion = st.selectbox("Which one do you like most", ["Beef", "Palm oil", "Soy"])
+st.write("You´ve chosen", opcion)
+
+if opcion == 'Soy':
+    soy = pd.read_csv('.\data\Soy\soy_clean.csv')
+    fig = px.scatter(soy, x='Entity', y='Feed', color='Year')
+    st.plotly_chart(fig)
+
+if st.button("Click here!"):
+    st.write("¡Presionaste el botón!")
