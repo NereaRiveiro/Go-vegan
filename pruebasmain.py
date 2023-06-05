dd44db61049e6632b161d52530e365a0d390b02a
Nerea Riveiro <nereaironhack@gmail.com>
2023-06-05 19:53:02 +0200

    primer prototipo de st decente pero unaesthetic

--- pruebasmain.py
+++ pruebasmain.py
@@ -18,6 +18,7 @@ oil_prod = pd.read_csv(r'Go-vegan-\data/Palm/oil_prod_clean.csv')
 oil_yield = pd.read_csv(r'Go-vegan-\data/Palm/oil_yield_clean.csv')
 pop = pd.read_csv(r'Go-vegan-\data/Population/population_clean.csv')
 
+# filtro para sacar lista de sólo paises
 todos = soy['Entity'].value_counts().index.to_list()
 paises = filtrar_paises(todos)
 
@@ -42,7 +43,7 @@ if __name__ == '__main__':
 
 st.header("Are you aware of your impact?")
 st.subheader("Discover your food choices environmental impact")
-st.text("We´ve extracted info abaout the 40 foods more tipical around the world. Let´s check some info")
+st.write("We´ve extracted info abaout 40 foods most tipical around the world. Let´s check some info")
 
 
 # Gráfico de todas las food
@@ -61,9 +62,9 @@ fig = go.Figure(data=go.Bar(
 ))
 
 fig.update_layout(
-    title='Suma de Emisiones de Alimentos por Entidad',
-    xaxis_title='Total de Emisiones de Alimentos',
-    yaxis_title='Entidad',
+    title='Global CO emisions by food',
+    xaxis_title='Total emisions',
+    yaxis_title='Food',
     legend=dict(x=1.02, y=1),
     height=500,
     width=800
@@ -77,33 +78,50 @@ nombre = st.text_input("What's your name?")
 st.write("¡Hi,", nombre, "!")
 
 
-
-opcion = st.selectbox("Which one are you interested in the most?", ["Beef", "Palm oil", "Soy"])
+# Food specifications
+opcion = st.selectbox("Which food are you interested in the most?", ["Beef", "Palm oil", "Soy"])
 st.write("You´ve chosen", opcion)
 
 if opcion == 'Soy':
-    fig = px.scatter(soy, x='Entity', y='Feed', color='Year')
+
+    st.write("Fun fact: There are health benefits to soy, which include providing a good source of heart-healthy fats, fiber, potassium and iron.")
+    st.write("But not everything is sunshine and rainbows. Soy is one of the main causes of deforestation in South America. But, what for is destined the soybean production?")
+
+    # Comparative graphic between soybeans for food, feed or processed
+    df = soy[soy['Year'].dt.year >= 2000]
+    # Sum "Food", "Feed" y "Processed"
+    totals = df[['Food', 'Feed', 'Processed']].sum()
+    # Figure
+    fig, ax = plt.subplots()
+    x_labels = ['Food', 'Feed', 'Processed']
+    x = range(len(x_labels))
+    bars = ax.bar(x, totals)
+    # Labels in bars
+    for bar in bars:
+        height = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width() / 2, height,
+                f'{height}', ha='center', va='bottom')
+    # Labels x exe
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels)
+    # Show
     st.plotly_chart(fig)
 
-    # Gráfico de hectáreas por países
+    st.write("Wow! It´s surprising the difference between them. Some examples from what soy can be processed are soy oil and soybean cake for basic animal feed protein")
 
-
-
+    st.write("Where are these macroharvest?")
+    # Graphic: hectares by country
     # Filtered for just countries
     df_filtered = soy[soy['Entity'].isin(paises)]
-
     # Grouped for country and year and mean the harvested area
     grouped = df_filtered.groupby(['Entity', 'Year'])['Area harvested'].mean().reset_index()
-
     # Order descendent and filter for top 5
     top_countries = grouped.groupby('Entity')['Area harvested'].sum().nlargest(5).index
     df_top_countries = grouped[grouped['Entity'].isin(top_countries)]
-
     # Create interactive graphic
     fig = px.line(df_top_countries, x='Year', y='Area harvested', color='Entity',
                 labels={'Year': 'Year', 'Area harvested': 'Area harvested (hectareas)'},
                 title='Evolution of hectares harvested by top 5 countries')
-
     # Animation
     fig.update_layout(
         xaxis=dict(range=[df_top_countries['Year'].min(), df_top_countries['Year'].max()], autorange=False),
@@ -127,7 +145,6 @@ if opcion == 'Soy':
             )
         ]
     )
-
     # Animation photograms
     frames = []
     for year in df_top_countries['Year'].unique():
@@ -138,11 +155,28 @@ if opcion == 'Soy':
             name=str(year)
         )
         frames.append(frame)
-
     fig.frames = frames
-
     # Show
     st.plotly_chart(fig)
+
+    st.write("In Brazil the production in 2020 was by 370k hectares. That´s the equivalent for 370k football fields or the whole surface of Mexico City")
+
+
+    # Tofu and soy milk consumption
+    soy_choice = st.selectbox("The most common human foods made by soy are soy milk and tofu, let´s check them!", ["Soy milk", "Tofu"])
+    if soy_choice == 'Soy milk':
+        soymilk_choice = st.selectbox("how often do you have it?", ["Once a day", "1-2 times a week", "Never"])
+        if soymilk_choice == "Once a day":
+            soy_milk_emissions = fod[fod['Entity'] == 'Soy milk']
+
+            # Calcular el total de las emisiones de 200g
+            total_emissions = soy_milk_emissions[['food_emissions_land_use', 'food_emissions_farm', 'food_emissions_animal_feed',
+                                                'food_emissions_processing', 'food_emissions_transport', 'food_emissions_retail',
+                                                'food_emissions_packaging', 'food_emissions_losses']].sum().sum() * 0.2
+            # Result
+            st.write("Total emisions of Soy milk by year", total_emissions.round(2)* 365)
+
+
 
 if st.button("Click here!"):
     st.write("¡Presionaste el botón!")
