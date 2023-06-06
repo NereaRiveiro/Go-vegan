15f9cca849a4f378d3d457fe2725b4f69ad80337
Nerea Riveiro <nereaironhack@gmail.com>
2023-06-06 18:14:22 +0200

    no mucho, atasco pero continuamos

--- pruebasmain.py
+++ pruebasmain.py
@@ -28,16 +28,47 @@ paises = filtrar_paises(todos)
 
 palette = ['#6a863b', '#68c843', '#f39f18']
 
-st.markdown(
-    f"""
+# Configuración del color de fondo
+with open('style.css') as f:
+    st.markdown(
+        """
     <style>
-    body {{
-        font-family: 'Arial', monospace;
-    }}
+    .reportview-container .markdown-text-container {
+        font-family: monospace;
+    }
+    .sidebar .sidebar-content {
+        background-image: linear-gradient(#2e7bcf,#2e7bcf);
+        color: white;
+    }
+    .Widget>label {
+        color: white;
+        font-family: monospace;
+    }
+    [class^="st-b"]  {
+        color: white;
+        font-family: monospace;
+    }
+    .st-bb {
+        background-color: transparent;
+    }
+    .st-at {
+        background-color: #0c0080;
+    }
+    footer {
+        font-family: monospace;
+    }
+    .reportview-container .main footer, .reportview-container .main footer a {
+        color: #0c0080;
+    }
+    header .decoration {
+        background-image: none;
+    }
+
     </style>
     """,
-    unsafe_allow_html=True
-)
+        unsafe_allow_html=True,
+    )
+
 
 def main():
     st.title("Go vegan?")
